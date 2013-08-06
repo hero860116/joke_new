8a6ccebecfd2cfdeced23e9f064288f299bf243f
hero860116 <hero_860116@hotmail.com>
2013-08-06 22:59:35 +0800

    init

---
+++ web/src/main/java/com/kelepi/dal/dao/HibernateBaseDAO.java
@@ -0,0 +1,19 @@
+package com.kelepi.dal.dao;
+
+import javax.persistence.Transient;
+
+import org.hibernate.Session;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+
+public class HibernateBaseDAO extends HibernateDaoSupport{
+
+	/**
+	 * 提供ao层获取外部session，控制缓存
+	 * @return
+	 */
+	@Transient
+	public Session getSessionForOut() {
+		return getSession();
+	}
+}
